7cb176d232236dc5816cd25f7427a1d7dca8a0a0
Vincent Zhao <vinzhao11@gmail.com>
2022-07-16 23:23:05 -0400

    7.16 Progress

---
+++ js185/todos_mine/lib/session-persistence.js
@@ -0,0 +1,38 @@
+const SeedData = require("./seed-data");
+const deepCopy = require("./deep-copy");
+const { sortTodoLists, sortTodos } = require("./sort");
+
+module.exports = class SessionPersistence {
+  constructor(session) {
+    this._todoLists = session.todoLists || deepCopy(SeedData);
+    session.todoLists = this._todoLists;
+  }
+
+	  // Are all of the todos in the todo list done? If the todo list has at least
+  // one todo and all of its todos are marked as done, then the todo list is
+  // done. Otherwise, it is undone.
+  isDoneTodoList(todoList) {
+    return todoList.todos.length > 0 && todoList.todos.every(todo => todo.done);
+  }
+
+  // Returns a copy of the list of todos in the indicated todo list by sorted by
+  // completion status and title (case-insensitive).
+  sortedTodos(todoList) {
+    let todos = todoList.todos;
+    let undone = todos.filter(todo => !todo.done);
+    let done = todos.filter(todo => todo.done);
+    return deepCopy(sortTodos(undone, done));
+  }
+
+	// Returns a copy of the todo list with the indicated ID. Returns `undefined`
+  // if not found. Note that `todoListId` must be numeric.
+  loadTodoList(todoListId) {
+    let todoList = this._todoLists.find(todoList => todoList.id === todoListId);
+    return deepCopy(todoList);
+  }
+
+// Does the todo list have any undone todos? Returns true if yes, false if no.
+hasUndoneTodos(todoList) {
+  return todoList.todos.some(todo => !todo.done);
+}
+};
