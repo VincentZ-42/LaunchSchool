02c8ccd79ddee9b567eef20ad42b5a1cf75cc90f
Vincent Zhao <vinzhao11@gmail.com>
2022-08-08 00:42:02 -0400

    8.7.2022 Progress

---
+++ js189/projects/hello-world/README.md
@@ -0,0 +1,4 @@
+use morgan for logging
+used layout.pug for repeitive views
+used app.use() for error handling
+optimized with parameterized routes
--- js189/projects/hello-world/hello.js
+++ js189/projects/hello-world/hello.js
@@ -1,37 +1,83 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = 3000;
+const COUNTRY_DATA = [
+  {
+    path: "/english",
+    flag: "flag-of-United-States-of-America.png",
+    alt: "US Flag",
+    title: "Go to US English site",
+  },
+  {
+    path: "/french",
+    flag: "flag-of-France.png",
+    alt: "Drapeau de la france",
+    title: "Aller sur le site français",
+  },
+  {
+    path: "/serbian",
+    flag: "flag-of-Serbia.png",
+    alt: "Застава Србије",
+    title: "Идите на српски сајт",
+  },
+  {
+    path: "/chinese",
+    flag: "flag-of-China.png",
+    alt: "中国国旗",
+    title: "去中国网站",
+  },
+  {
+    path: "/japanese",
+    flag: "flag-of-Japan.png",
+    alt: "日本の国旗",
+    title: "日本のサイトへ",
+  },
+];
+
+const LANGUAGE_CODES = {
+  english: "en-US",
+  french: "fr-FR",
+  serbian: "sr-Cryl-rs",
+  chinese: "zh-CN",
+  japanese: "ja-JP",
+};
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
+app.use(morgan("common"));
+
+app.locals.currentPathClass = (path, currentPath) => {
+  return path === currentPath ? "current" : "";
+};
 
 app.get('/', (req, res) => {
-	res.redirect('/english');
+  res.redirect('/english');
 });
 
-app.get('/english', (req, res) => {
-	res.render('hello-world-english');
+app.get('/:language', (req, res, next) => {
+  const language = req.params.language;
+  const languageCode = LANGUAGE_CODES[language];
+
+  if (!languageCode) {
+    next(new Error(`Language not supported: ${language}`));
+  } else {
+    res.render(`hello-world-${language}`, {
+      currentPath: req.path,
+      countries: COUNTRY_DATA,
+      language: LANGUAGE_CODES[language],
+    });
+  }
 });
 
-app.get('/french', (req, res) => {
-	res.render('hello-world-french');
-});
-
-app.get('/serbian', (req, res) => {
-	res.render('hello-world-serbian');
-});
-
-app.get('/chinese', (req, res) => {
-	res.render('hello-world-chinese');
-});
-
-app.get('/japanese', (req, res) => {
-	res.render('hello-world-japanese');
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
 });
 
 app.listen(PORT, "localhost", () => {
-	console.log(`Listening on PORT: ${PORT}`);
+  console.log(`Listening on PORT: ${PORT}`);
 });
