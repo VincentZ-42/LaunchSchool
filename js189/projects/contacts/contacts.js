a129f5f43249a6bc8ee36a7be4357892a5d81e4d
Vincent Zhao <vinzhao11@gmail.com>
2022-08-09 23:11:09 -0400

    8.9.2022

--- js189/projects/contacts/contacts.js
+++ js189/projects/contacts/contacts.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
-const PORT = 3000;
+const express = require("express"); // This is so we can use express for handling our routing
+const morgan = require("morgan");   // This is for using mordule to help with logging
+const app = express();              // This is the object of our backend server
+const PORT = 3000;                  // local environment to use as PORT
 
 let contactData = [
   {
@@ -42,11 +42,13 @@ const sortContacts = contacts => {
   });
 };
 
-app.set("views", "./views");
-app.set("view engine", "pug");
+app.set("views", "./views");                        // server knows where to look for views
+app.set("view engine", "pug");                      // designates pug as our view template engine
 
-app.use(express.static("public"));
-app.use(morgan("common"));
+app.use(express.static("public"));                  // Looks in our public folder for static files
+app.use(express.urlencoded({ extended: false }));   // tells express to expect form data in URL-encoded format
+                                                    // - Allows us to accept form data and use req.body
+app.use(morgan("common"));                          // Uses common logging from morgan module
 
 app.get("/", (req, res) => {
   res.redirect("/contacts");
@@ -59,8 +61,92 @@ app.get("/contacts", (req, res) => {
 });
 
 app.get("/contacts/new", (req, res) => {
-  res.render("newContact");
+  res.render("new-contact");
 });
+
+const isAlphabetic = text => /^[a-z]+$/i.test(text);
+
+app.post("/contacts/new",
+  // middleware thaat initates array to hold errors in res.locals (data passed between middleware)
+  (req, res, next) => {
+    res.locals.errorMessages = [];
+    next();
+  },
+  // middleware to trim all whitespaces from inputs
+  (req, res, next) => { // trim whitespace
+    res.locals.firstName   = req.body.firstName.trim();
+    res.locals.lastName    = req.body.lastName.trim();
+    res.locals.phoneNumber = req.body.phoneNumber.trim();
+    next();
+  },
+  // Validates first name
+  (req, res, next) => {
+    let firstName = res.locals.firstName;
+    if (firstName.length === 0) {
+      res.locals.errorMessages.push("First name is required.");
+    } else if (firstName.length > 25) {
+      res.locals.errorMessages.push("First name is too long. Maximum length is 25 characters.");
+    } else if (!isAlphabetic(firstName)) {
+      res.locals.errorMessages.push("First name contains invalid characters. The name must be alphabetic.");
+    }
+    next();
+  },
+  // Valdiates last name
+  (req, res, next) => {
+    let lastName = res.locals.lastName;
+    if (lastName.length === 0) {
+      res.locals.errorMessages.push("Last name is required.");
+    } else if (lastName.length > 25) {
+      res.locals.errorMessages.push("Last name is too long. Maximum length is 25 characters.");
+    } else if (!isAlphabetic(lastName)) {
+      res.locals.errorMessages.push("Last name contains invalid characters. The name must be alphabetic.");
+    }
+    next();
+  },
+  // Validates phone number
+  (req, res, next) => {
+    if (req.body.phoneNumber.length === 0) {
+      res.locals.errorMessages.push("Phone Number is required.");
+    } else if (!/^\d\d\d-\d\d\d-\d\d\d\d$/.test(req.body.phoneNumber)) {
+      res.locals.errorMessages.push("Invalid phone number format. Use ###-###-####");
+    }
+    next();
+  },
+  // middleware that checks for duplicates
+  (req, res, next) => {
+    let fullName = `${res.locals.firstName} ${res.locals.lastName}`;
+    let foundContact = contactData.find(contact => {
+      return `${contact.firstName} ${contact.lastName}` === fullName;
+    });
+
+    if (foundContact) {
+      res.locals.errorMessages.push(`${fullName} is already on your contact list. Duplicates are not allowed.`);
+    }
+
+    next();
+  },
+  // Checks all previous validation and renders form with error messages
+  (req, res, next) => {
+    if (res.locals.errorMessages.length > 0) {
+      res.render("new-contact", {
+        errorMessages: res.locals.errorMessages,
+      });
+    } else {
+      next();
+    }
+  },
+  // Saves data if no errors and returns to contacts page
+  (req, res) => {
+    // contactData.push({ ...req.body })
+    // More common way of data extraction using spread syntax ^^^
+    contactData.push({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phoneNumber: req.body.phoneNumber
+    });
+    res.redirect("/contacts");
+  }
+);
 
 app.listen(PORT, "localhost", () => {
   console.log("Listening to port 3000.");
